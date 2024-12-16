92c12aefb31a0c14eb32e688a4487d941c1308b4
StereoZing <94567076+StereoZing@users.noreply.github.com>
2024-12-16 08:13:09 +0300

    Add files via upload

---
+++ WebProject/cart/script.js
@@ -0,0 +1,80 @@
+﻿window.addEventListener("DOMContentLoaded", function () {
+    document.body.style.backgroundImage = "url(../source/BG.jpg)";
+    const cards = document.querySelector("#cards");
+    const row = cards.children[0].cloneNode(true);
+    cards.innerHTML = '';
+    const elDate = document.querySelector("input[type=date]");
+    elDate.value = new Date().toLocaleDateString("en-CA");
+    if (sessionStorage.length == 0) {
+        document.querySelector("form").addEventListener("submit", function (event) {
+            event.preventDefault();
+        });
+        return;
+    }
+    const evileye = document.querySelector("#evileye");
+    evileye.classList.remove("d-block");
+    evileye.classList.add("d-none");
+    let sum = 0;
+    for (let i = 0; i < sessionStorage.length; i++) {
+        let key = sessionStorage.key(i);
+        let obj = JSON.parse(sessionStorage.getItem(key));
+        let newrow = row.cloneNode(true);
+        newrow.querySelector("img").src = "../" + obj.img;
+        newrow.querySelector("h3").innerHTML = key;
+        const origrow = newrow.querySelector("div.row");
+        const parentrow = origrow.parentElement;
+        parentrow.removeChild(origrow);
+        let sumi = 0;
+        for (let j = 0; j < obj.cartArr.length; j++) {
+            sumi += obj.cartArr[j] * obj.priceArr[j];
+            const currentrow = origrow.cloneNode(true);
+            currentrow.children[0].innerHTML = obj.priceArr[j] + "руб";
+            currentrow.children[1].innerHTML = obj.cartArr[j] + "штук";
+            parentrow.appendChild(currentrow);
+        }
+        sum += sumi;
+        origrow.children[0].innerHTML = "Итого: ";
+        origrow.children[1].innerHTML = sumi + "руб";
+        parentrow.appendChild(origrow);
+        cards.appendChild(newrow);
+    }
+    elDate.nextElementSibling.value = sum + "руб";
+    document.querySelector("form").addEventListener("submit", function (event) {
+        event.preventDefault();
+        const button = this.querySelector(".btn");
+        if (button.disabled) {
+            return;
+        }
+        button.disabled = true;
+        let httpRequest = new XMLHttpRequest();
+        //временный адрес
+        httpRequest.open("POST", "https://formcarry.com/s/77refVsM9Xy");
+        httpRequest.setRequestHeader("Content-Type", "application/json");
+        httpRequest.setRequestHeader("Accept", "application/json");
+        let obj = {};
+        let inputs = document.querySelectorAll("form > *");
+        inputs.forEach(function (i) {
+            if (i.name !== "") {
+                obj[i.name] = i.value;
+            }
+        });
+        httpRequest.send(JSON.stringify(obj));
+        httpRequest.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                const nouspex = document.querySelector(".nouspex");
+                const uspex = document.querySelector(".uspex");
+                if (this.status === 200) {
+                    nouspex.classList.add("d-none");
+                    uspex.classList.remove("d-none");
+                    sessionStorage.clear();
+                    cards.innerHTML = '';
+                    document.querySelector("form").classList.add("d-none");
+                    document.querySelector(".zakaz").classList.add("d-none");
+                } else {
+                    nouspex.classList.remove("d-none");
+                }
+                button.disabled = false;
+            }
+        };
+    });
+});
---
+++ WebProject/js/Card.js
@@ -0,0 +1,79 @@
+﻿let list;
+
+class Card {
+    constructor(name, opisanie, img, grammArr, priceArr, animalsArr, element) {
+        this.cartArr = new Array(grammArr.length).fill(0);
+        this.name = name;
+        this.opisanie = opisanie;
+        this.img = img;
+        this.grammArr = grammArr;
+        this.priceArr = priceArr;
+        this.animalsArr = animalsArr;
+        if (element === null) {
+            let tovar = document.getElementById("tovar");
+            let cardi = tovar.children[0].cloneNode(true);
+            tovar.appendChild(cardi);
+            this.element = cardi.children[0];
+        } else {
+            this.element = element;
+        }
+        let body = this.element.children[1];
+        body.children[1].addEventListener("click", this.createModal.bind(this));
+        this.update(body);
+    }
+
+    update(body) {
+        let option = document.createElement("option");
+        option.value = this.name;
+        list.appendChild(option);
+        this.element.id = this.name.toLowerCase();
+        this.element.children[0].src = this.img;
+        body.children[0].innerHTML = this.name;
+    }
+
+    createModal() {
+        let detalno = document.getElementById("detalno");
+        detalno.querySelector(".modal-title").textContent = this.name;
+        detalno.querySelector(".opisanie").textContent = this.opisanie;
+        let container = detalno.querySelector(".container-fluid");
+        let row = container.children[0].cloneNode(true);
+        container.textContent = '';
+        container.appendChild(row);
+        this.fillModal(row, 0);        
+        for (let i = 1; i < this.grammArr.length; i++) {            
+            let newrow = row.cloneNode(true);
+            container.appendChild(newrow);
+            this.fillModal(newrow, i);
+        }
+        window.history.pushState({ modal: "true" }, "", "#" + this.name);
+    }
+
+    fillModal(row, index) {
+        row.children[0].textContent = this.grammArr[index] + "гр";
+        row.children[1].textContent = this.priceArr[index] + "руб";
+        row.children[3].innerHTML = this.cartArr[index];
+        row.children[2].addEventListener("click", function () {
+            if (this.cartArr[index] > 0) {
+                this.cartArr[index] -= 100;
+                this.updateRow(row, index);
+                if (this.cartArr.every(x => x === 0)) {
+                    sessionStorage.removeItem(this.name);
+                }
+            }
+        }.bind(this));
+        row.children[4].addEventListener("click", function () {
+            this.cartArr[index] += 100;
+            this.updateRow(row, index);
+        }.bind(this));
+    }
+
+    updateRow(row, index) {
+        row.children[3].innerHTML = this.cartArr[index];
+        sessionStorage.setItem(this.name, JSON.stringify(this, function (k, v) {
+            if (!(k === "opisanie" || k === "name" || k === "element" || k === "animalsArr")) {
+                return v;
+            }
+            return undefined;
+        }));
+    }
+}
